4f94f31437606d0edade2e5ff9042e6912da4651
Siva Santhosh <sivasanthosh.g@teleapps.com>
2024-05-13 16:39:09 +0530

    liquibase commit

---
+++ src/main/java/com/db/operations/DataBaseLiquibaseDemoApplication.java
@@ -0,0 +1,14 @@
+package com.db.operations;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataBaseLiquibaseDemoApplication {
+
+	public static void main(String[] args) {
+		System.out.println("Learning integration of liquibase demo");
+		SpringApplication.run(DataBaseLiquibaseDemoApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/db/operations/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.db.operations;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(DataBaseLiquibaseDemoApplication.class);
+	}
+
+}
